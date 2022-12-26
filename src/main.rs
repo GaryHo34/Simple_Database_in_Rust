58a4faa8c190c9f3b2affc2fea3499b8d688480a
Gary Ho <garyho0916@gmail.com>
2022-12-26 14:37:48 -0800

    Part1: Making a Simple REPL
    Part1 Making a Simple REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    loop {
+        // read_line will append the string to the buffer
+        // clean the buffer before stores the command
+        buffer.clear();
+        print_prompt();
+        read_input(&mut buffer);
+        if buffer == ".exit" {
+            return;
+        } else {
+            println!("Unrecognized command {buffer}.")
+        }
+    }
+}
+
+fn print_prompt() {
+    print!("db > ");
+    // force to flush stdout and display the output.
+    // flush is a trait from Write.
+    io::stdout().flush().expect("Flush Failed");
+}
+
+fn read_input(buffer: &mut String) {
+    let stdin = io::stdin();
+    let bytes_read = stdin.read_line(buffer).expect("Error reading input");
+    if bytes_read == 0 {
+        panic!("Error reading input");
+    }
+    // to remove the last newline char
+    buffer.pop();
 }
