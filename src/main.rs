e0fd9d4c89db5dbe329f57fa1e623d9e1d13182e
Gary Ho <garyho0916@gmail.com>
2023-01-02 13:30:40 -0800

    Part3 In-Memory Single-Table Database

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 use std::{
     io::{self, Write},
-    process::exit,
+    process,
 };
+
+const ID_SIZE: usize = 4;
+const USERNAME_SIZE: usize = 32;
+const EMAIL_SIZE: usize = 255;
+
+const ROW_SIZE: usize = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+const PAGE_SIZE: usize = 4096;
+const TABLE_MAX_PAGES: usize = 100;
+const ROWS_PER_PAGE: usize = PAGE_SIZE / ROW_SIZE;
+const TABLE_MAX_ROWS: usize = TABLE_MAX_PAGES * ROWS_PER_PAGE;
+
+enum ExecuteErrorType {
+    ExecuteTableFull,
+}
+
+enum MetaCommandErrorType {
+    MetaCommandUnrecognizedCommand,
+}
+
+enum PrepareErrorType {
+    PrepareSyntaxError,
+    PrepareUnrecognizedStatement,
+}
 
 enum StatementType {
     Insert,
@@ -9,59 +32,75 @@ enum StatementType {
 }
 
 struct Statement {
-    state: StatementType,
+    statement_type: StatementType,
+    row_to_insert: Option<Row>,
 }
 
-impl Statement {
-    fn new(buffer: &String) -> Result<Statement, ()> {
-        if buffer == "INSERT" {
-            return Ok(Statement {
-                state: StatementType::Insert,
-            });
-        } else if buffer == "SELECT" {
-            return Ok(Statement {
-                state: StatementType::Select,
-            });
-        }
-        Err(())
-    }
+struct Row {
+    id: u32,
+    username: String,
+    email: String,
+}
 
-    fn execute(&self) {
-        match self.state {
-            StatementType::Insert => println!("This is where we would do an insert."),
-            StatementType::Select => println!("This is where we would do a select."),
+struct Page {
+    rows: Vec<Row>,
+}
+
+impl Page {
+    fn new() -> Self {
+        Page {
+            rows: Vec::<Row>::with_capacity(ROWS_PER_PAGE),
+        }
+    }
+}
+
+struct Table {
+    num_rows: usize,
+    pages: Vec<Page>,
+}
+
+impl Table {
+    fn new() -> Self {
+        Table {
+            num_rows: 0,
+            pages: Vec::<Page>::with_capacity(TABLE_MAX_PAGES),
         }
     }
 }
 
 fn main() {
-    let mut buffer = String::new();
+    let mut table = Table::new();
     loop {
         // read_line will append the string to the buffer
-        // clean the buffer before stores the command
-        buffer.clear();
         print_prompt();
-        read_input(&mut buffer);
+        let input_buffer = read_input();
 
         // We dont need MetaCommandResult, use built-in result to do this
-        if buffer.as_bytes()[0] as char == '.' {
-            match do_meta_command(&buffer) {
+        if input_buffer.starts_with('.') {
+            match do_meta_command(&input_buffer) {
                 Ok(()) => println!("Success!"),
-                Err(()) => println!("Unrecognized Command!"),
+                Err(_) => println!("Unrecognized Command!"),
             }
             continue;
         }
 
-        let statement = match Statement::new(&buffer) {
+        let statement = match prepare_statement(&input_buffer) {
             Ok(val) => val,
             // If err just to a new loop
-            Err(()) => {
+            Err(_) => {
                 println!("Unrecognized Command!");
                 continue;
             }
         };
 
-        statement.execute();
+        match execute_statement(&statement, &mut table) {
+            Ok(()) => {
+                println!("Executed.");
+            }
+            Err(_) => {
+                println!("Error: Table full.");
+            }
+        }
     }
 }
 
@@ -72,22 +111,99 @@ fn print_prompt() {
     io::stdout().flush().expect("Flush Failed");
 }
 
-fn read_input(buffer: &mut String) {
-    let stdin = io::stdin();
-    let bytes_read = stdin.read_line(buffer).expect("Error reading input");
-    if bytes_read == 0 {
-        panic!("Error reading input");
-    }
+fn read_input() -> String {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Error reading input");
+
     // to remove the last newline char
-    buffer.pop();
+    String::from(buffer.trim())
 }
 
-fn do_meta_command(buffer: &String) -> Result<(), ()> {
-    if buffer == ".exit" {
-        exit(0);
-    } else if buffer == ".ex" {
-        // do something
-        return Ok(());
+fn do_meta_command(input_buffer: &String) -> Result<(), MetaCommandErrorType> {
+    if input_buffer.eq(".exit") {
+        process::exit(0);
     }
-    Err(())
+
+    Err(MetaCommandErrorType::MetaCommandUnrecognizedCommand)
+}
+
+fn prepare_statement(input_buffer: &String) -> Result<Statement, PrepareErrorType> {
+    if input_buffer.to_lowercase().starts_with("insert") {
+        let args_assigned: Vec<&str> = input_buffer.split(" ").collect();
+        if args_assigned.len() < 4 {
+            return Err(PrepareErrorType::PrepareSyntaxError);
+        }
+        return Ok(Statement {
+            statement_type: StatementType::Insert,
+            row_to_insert: Some(Row {
+                id: args_assigned[1].trim().parse().unwrap(),
+                username: String::from(args_assigned[2].trim()),
+                email: String::from(args_assigned[3].trim()),
+            }),
+        });
+    } else if input_buffer.to_lowercase().starts_with("select") {
+        return Ok(Statement {
+            statement_type: StatementType::Select,
+            row_to_insert: None,
+        });
+    }
+    Err(PrepareErrorType::PrepareUnrecognizedStatement)
+}
+
+fn row_slot(table: &mut Table, row_num: usize) -> &mut Row {
+    let page_num = row_num / ROWS_PER_PAGE;
+    let row_offset = row_num % ROWS_PER_PAGE;
+    if page_num >= table.pages.len() {
+        table.pages.push(Page::new());
+    }
+    let row = table.pages[page_num].rows.get_mut(row_offset);
+    match row {
+        Some(_) => (),
+        None => {
+            table.pages[page_num].rows.push(Row {
+                id: 0,
+                username: String::new(),
+                email: String::new(),
+            });
+        }
+    }
+    &mut table.pages[page_num].rows[row_offset]
+}
+
+fn execute_statement(statement: &Statement, table: &mut Table) -> Result<(), ExecuteErrorType> {
+    match statement.statement_type {
+        StatementType::Insert => {
+            return execute_insert(statement, table);
+        }
+        StatementType::Select => {
+            return execute_select(table);
+        }
+    }
+}
+
+fn execute_insert(statement: &Statement, table: &mut Table) -> Result<(), ExecuteErrorType> {
+    if table.num_rows >= TABLE_MAX_ROWS {
+        return Err(ExecuteErrorType::ExecuteTableFull);
+    }
+    let row = row_slot(table, table.num_rows);
+    match &statement.row_to_insert {
+        Some(row_to_insert) => {
+            row.id = row_to_insert.id;
+            row.username = String::from(&row_to_insert.username);
+            row.email = String::from(&row_to_insert.email);
+            table.num_rows += 1;
+        },
+        None=>()
+    }
+    Ok(())
+}
+
+fn execute_select(table: &mut Table) -> Result<(), ExecuteErrorType> {
+    for i in 0..table.num_rows {
+        let row = row_slot(table, i);
+        println!("{} {} {}", row.id, row.username, row.email);
+    }
+    Ok(())
 }
